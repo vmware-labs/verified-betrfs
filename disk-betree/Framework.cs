b7650a6ed1833700160e0f46a640db092ab57800
Travis Hance <thance@vmware.com>
2019-06-28 00:33:28 -0700

    add skeleton Framework.cs for system-y disk code

---
+++ disk-betree/Framework.cs
@@ -0,0 +1,10 @@
+using _5_DiskInterface_Compile; // TODO what to do about mangled namespaces?
+
+class DiskIOHandlerImpl : DiskIOHandler {
+  public void write(uint lba, Dafny.Sequence<byte> sector) {
+    // TODO
+  }
+  public void read(uint lba, out Dafny.Sequence<byte> sector) {
+    // TODO
+  }
+}
