35d416a01a2645e9bf361e3d957843970e1dc885
Travis Hance <thance@vmware.com>
2019-06-29 00:25:15 -0700

    work on Framework.cs

--- disk-betree/Framework.cs
+++ disk-betree/Framework.cs
@@ -1,10 +1,34 @@
-using _5_DiskInterface_Compile; // TODO what to do about mangled namespaces?
+using _20_DiskInterface_Compile; // TODO what to do about mangled namespaces?
+using _84_Impl_Compile;
+
+using System;
+using System.IO;
 
 class DiskIOHandlerImpl : DiskIOHandler {
   public void write(uint lba, Dafny.Sequence<byte> sector) {
-    // TODO
+    File.WriteAllBytes(getFilename(lba), sector.Elements);
   }
+
   public void read(uint lba, out Dafny.Sequence<byte> sector) {
-    // TODO
+    string filename = getFilename(lba);
+    byte[] bytes = File.ReadAllBytes(filename);
+    if (bytes.Length != 1024*1024) {
+      throw new Exception("Invalid block at " + filename);
+    }
+    sector = new Dafny.Sequence<byte>(bytes);
+  }
+
+  private string getFilename(uint lba) {
+    return "filesystem/" + lba.ToString("X8");
+  }
+}
+
+class Framework {
+  public static void Main(string[] args) {
+    World w = new World();
+    w.diskIOHandler = new DiskIOHandlerImpl();
+    w.s = InitVariables();
+
+    handle(InitConstants(), w);
   }
 }
