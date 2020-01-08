a26c43f46be0730690ca4319bae82e0ce9a614d6
Travis Hance <tjhance7@gmail.com>
2020-01-08 15:09:48 -0500

    update the dafny commit pointer to latest cpp branch

--- tools/install-dafny.sh
+++ tools/install-dafny.sh
@@ -13,10 +13,11 @@ mono ./nuget.exe restore Source/Boogie.sln
 msbuild /p:Configuration=Release Source/Boogie.sln
 cd ..
 
-#git clone --branch fast-array-slicing https://github.com/secure-foundations/dafny.git
 git clone https://github.com/secure-foundations/dafny.git
 cd dafny
-git checkout 3a1533e99d7a568686630b99c88fa12dc1b0ba08
+
+# cpp branch
+git checkout f13105bce7b87b4c889f325e7a74d894c9d5e0d0
 
 msbuild /p:Configuration=Release Source/Dafny.sln
 cd ..
