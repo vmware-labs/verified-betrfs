5669aa6908de4fd402865732e34a7b1cf946cdb9
Travis Hance <tjhance7@gmail.com>
2020-01-08 19:31:59 -0500

    add more documentation to the script

--- tools/checkout-dafny-commit.sh
+++ tools/checkout-dafny-commit.sh
@@ -3,6 +3,18 @@
 ### Update this commit hash in order to update the version of dafny
 ### to build Veribetrfs with.
 
+### This script is called as a subroutine from our installer
+### and updater scripts, and it serves as the single point to save
+### the dafny commit hash compatible with the veribetrfs repository.
+
+### Typically, we'll expect to keep this set to the head of the cpp
+### branch of our dafny fork.
+
+### When the dafny runtime API on that branch changes, we can update
+### our code in this repo while updating this commit hash. Hence,
+### this hash should always point to a version of dafny comaptible
+### with this version of veribetrfs.
+
 set -e
 set -x
 
