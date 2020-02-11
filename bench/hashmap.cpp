b7d6bd84ac5af1db931ca5678ad80de819a6e41d
Travis Hance <tjhance7@gmail.com>
2020-02-10 19:23:37 -0500

    get benchmark working

--- bench/hashmap.cpp
+++ bench/hashmap.cpp
@@ -2,13 +2,17 @@
 //#include <Framework.h>
 //#include <Bundle.i.h>
 #include "MutableMapImpl.cpp"
+#include <iostream>
 
 using namespace _29_MutableMap_Compile;
 
 int main(int argc, char* argv[]) {
     ResizingHashMap<uint64> m;
+    m.__ctor(1024);
 
     for (uint64 v = 0; v <= 1000000; v++) {
         m.Insert(v, v);
     }
+
+    cout << m.Get(5).is_Some() << endl;
 }
