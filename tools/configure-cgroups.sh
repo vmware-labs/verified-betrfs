05133758ecb08304d42f52bd1ce98cc272cef8de
Travis Hance <tjhance7@gmail.com>
2020-02-17 22:59:12 -0500

    set swappiness to 0

--- tools/configure-cgroups.sh
+++ tools/configure-cgroups.sh
@@ -7,3 +7,4 @@ set -e
 MEM=`python -c "print 4 * 1024 * 1024 * 1024"`
 
 echo $MEM > /sys/fs/cgroup/memory/VeribetrfsExp/memory.limit_in_bytes
+echo 0    > /sys/fs/cgroup/memory/VeribetrfsExp/memory.swappiness
