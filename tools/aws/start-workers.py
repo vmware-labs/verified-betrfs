54f65d79c172253e7d43620ad8c0a43a89020a84
Andrea Lattuada <andrea@lattuada.me>
2020-05-11 01:12:04 +0200

    aws experiments part 1

---
+++ tools/aws/start-workers.py
@@ -0,0 +1,17 @@
+import urllib, json, sys
+import getpass
+# import requests # 'pip install requests'
+import boto3 # AWS SDK for Python (Boto3) 'pip install boto3'
+from botocore.exceptions import ClientError
+
+ec2_connection = boto3.client('ec2', region_name='us-east-2')
+
+try:
+    response = ec2_connection.start_instances(InstanceIds=[
+        'i-0417debb8c8b1f5f6'
+        ])
+    print(response)
+except ClientError as e:
+    print(e)
+
+
