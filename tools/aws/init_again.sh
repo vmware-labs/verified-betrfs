fe701924b0126c739aa1d8f2e821f74df795a84b
Andrea Lattuada <andrea@lattuada.me>
2020-05-11 21:55:40 +0200

    new scale workers instance ids

--- tools/aws/init_again.sh
+++ tools/aws/init_again.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
-sudo mkfs.ext4 /dev/xvde
-sudo mount /mnt/xvde
-sudo chown ubuntu:ubuntu /mnt/xvde
+
+mkfs.ext4 /dev/xvde
+mount /mnt/xvde
+chown ubuntu:ubuntu /mnt/xvde
+cgcreate -a ubuntu:ubuntu -t ubuntu:ubuntu -g memory:VeribetrfsExp
