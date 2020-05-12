4bef738ca3106232646287d462bb1a2c10f89e28
Andrea Lattuada <andrea@lattuada.me>
2020-05-12 12:04:03 +0200

    describe-instances

--- tools/aws/describe-instances.py
+++ tools/aws/describe-instances.py
@@ -1,23 +1,38 @@
 #!/usr/bin/python3
+import argparse
 import urllib, json, sys
 import boto3
 import pprint
+from collections import namedtuple
 from botocore.exceptions import ClientError
 
-instance_ids = [
-    'i-0ef45a89f48487fce',
-    'i-006858073c1785f92',
-    'i-0070ff5413bf8eed4',
-    'i-0119b98c9b74ffaea'
-]
+parser = argparse.ArgumentParser()
+parser.add_argument('--all', action='store_true')
+parser.add_argument('--ssh', action='store_true')
+args = parser.parse_args()
 
-def prettyResponse(response):
-    return pprint.pprint(response, indent=2)
+Instance = namedtuple('Instance', ['Name', 'InstanceId', 'PublicIpAddress', 'State'])
 
 ec2_connection = boto3.client('ec2', region_name='us-east-2')
 try:
     response = ec2_connection.describe_instances()
-    print(prettyResponse(response))
+    # print(prettyResponse(response))
+
+    def extract_salient(inst):
+        return Instance(
+                Name=[x for x in inst['Tags'] if x['Key'] == 'Name'][0]['Value'],
+                InstanceId=inst['InstanceId'],
+                PublicIpAddress=inst['PublicIpAddress'],
+                State=inst['State']['Name'])
+    insts = [extract_salient(x) for reservation in response['Reservations'] for x in reservation['Instances']]
+    insts.sort(key=lambda x: x.Name)
+    if not args.all:
+        insts = [x for x in insts if x.Name.startswith('veri-worker')]
+    if args.ssh:
+        for ist in insts:
+            print("\033[1m{}\033[0m\tssh ubuntu@{}".format(ist.Name, ist.PublicIpAddress))
+    else:
+        pprint.pprint(insts, indent = 2)
 except ClientError as e:
     print(e)
 
