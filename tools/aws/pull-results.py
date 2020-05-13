036205e186489ba98a847bf2c724c1351e3f84f8
Jon Howell <howell@vmware.com>
2020-05-13 09:48:27 -0700

    launch automation

---
+++ tools/aws/pull-results.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import subprocess
+from automation import *
+
+def launch_worker_pipes(workers):
+    worker_pipes = []
+    for workeri in range(len(workers)):
+        worker = workers[workeri]
+
+        cmd = ["rsync", "-rav", "-e", " ".join(ssh_preamble()), ssh_target_for_worker(worker)+":expresults/", "expresults/"]
+        print("launching worker %d on %s: %s" % (workeri, worker["Name"], " ".join(cmd)))
+        worker_pipe = subprocess.Popen(cmd)
+        worker_pipes.append(worker_pipe)
+    return worker_pipes
+
+def main():
+    workers = retrieve_running_workers()
+    worker_pipes = launch_worker_pipes(workers)
+    monitor_worker_pipes(worker_pipes)
+    print("All jobs complete.")
+
+main()
