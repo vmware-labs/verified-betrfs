fa478903a88712e050c20e5f1a6949f5d300f4d8
Jon Howell <howell@vmware.com>
2020-05-13 22:27:33 -0700

    prep a big recordcount exp

--- tools/aws/launch-experiments.py
+++ tools/aws/launch-experiments.py
@@ -3,18 +3,24 @@
 from automation import *
 from suite import *
 
-suite = Suite(
-    "recordcount-page-19",
-#    Variable("git_branch", "git_branch", [Value("page", "page-la2"), Value("block", "leak-adventure-2")]),
-    Variable("git_branch", "git_branch", [Value("page", "page-la2")]),
-#    Variable("system", "run_veri", [Value("veri64k", "config-64kb"), Value("rocks", "rocks")]),
-    Variable("system", "run_veri", [Value("veri64k", "config-64kb")]),
-    Variable("ram", "run_veri", [Value("2gb", "ram=2.0b")]),
-    Variable("nodeCountFudge", "run_veri", [Value(str(f), "nodeCountFudge="+str(f)) for f in [0.8,0.9,1.0,1.1,1.2,1.3]]),
+common_vars = [
+    Variable("ram", "run_veri", [Value("2gb", "ram=2.0gb")]),
     Variable("device", "run_veri", [Value("disk", "device=disk")]),
-    Variable("workload", "run_veri", [Value("wka1m", "workload=ycsb/wka-uniform-rc1000k.spec")]),
+    Variable("workload", "run_veri", [Value("wka6m", "workload=ycsb/wka-uniform-rc6000k.spec")]),
     Variable("duration", "run_veri", [Value("1h", "time_budget=1h")]),
-    )
+    ]
+veri_suite = Suite(
+    "veri",
+    Variable("git_branch", "git_branch", [Value("page", "page-la2")]),
+    Variable("system", "run_veri", [Value("veri64k", "config-64kb")]),
+    Variable("nodeCountFudge", "run_veri", [Value(str(f), "nodeCountFudge="+str(f)) for f in [0.8,0.9,1.0,1.1,1.2,1.3]]),
+    *common_vars)
+blocks_suite = Suite(
+    "recordcount-page-22",
+    Variable("git_branch", "git_branch", [Value("block", "leak-adventure-2")]),
+    Variable("system", "run_veri", [Value("rocks", "rocks"), Value("veri64k", "config-64kb")]),
+    *common_vars)
+suite = ConcatSuite("recordcount-page-22", veri_suite, blocks_suite)
 
 RUN_VERI_PATH="tools/run-veri-config-experiment.py"
 
@@ -30,7 +36,7 @@ def cmd_for_idx(idx, worker):
 
 def main():
     set_logfile(suite.logpath())
-    log("PLOT %s" % suite.plot_command())
+    log("PLOT tools/aws/pull-results.py && %s && eog %s" % (suite.plot_command(), suite.png_filename()))
     log("VARIANTS %s" % suite.variants)
 
     workers = retrieve_running_workers()
