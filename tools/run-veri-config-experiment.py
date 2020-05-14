817f214f8cac9e4e1948b3febddabff87f7fa369
Jon Howell <howell@vmware.com>
2020-05-14 11:58:10 -0700

    cgroup disable flag for leak tests

--- tools/run-veri-config-experiment.py
+++ tools/run-veri-config-experiment.py
@@ -140,6 +140,7 @@ def main():
   log_stats = False
   nodeCountFudge = 1.0  # Pretend we have more memory when computing node count budget, since mean node is 75% utilized. This lets us tune veri to exploit all available cgroup memory.
   max_children = None   # Default
+  cgroup_enabled = True
 
   rocks = None
   time_budget_sec = 3600*24*365 # You get a year if you don't ask for a budget
@@ -179,6 +180,9 @@ def main():
                     3600 if unit=="h" else None))
       assert mult, "time_budget needs a unit"
       time_budget_sec = float(val_str[:-1]) * mult
+    elif arg.startswith("cgroup="):
+      enabled = arg.split("=")[1]
+      cgroup_enabled = enabled=="True"
     elif arg.startswith("output="):
       outpath = arg.split("=")[1]
       assert not os.path.exists(outpath)
@@ -274,7 +278,8 @@ def main():
   # See https://linux.die.net/man/1/taskset
   taskset_cmd = "taskset 4 "
 
-  command = taskset_cmd + "cgexec -g memory:VeribetrfsExp ./" + exe + " " + wl + " " + loc + " " + cmdoption
+  cgroup_prefix = "cgexec -g memory:VeribetrfsExp " if cgroup_enabled else ""
+  command = taskset_cmd + cgroup_prefix + "./" + exe + " " + wl + " " + loc + " " + cmdoption
   actuallyprint(command)
   sys.stdout.flush()
 
