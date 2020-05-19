48047854000756cc2ebe59d3d9d28bca3a9f2bf3
Andrea Lattuada <andrea@lattuada.me>
2020-05-19 16:22:55 +0200

    update-dafny works on macos again

--- tools/update-dafny.sh
+++ tools/update-dafny.sh
@@ -6,6 +6,6 @@ set -x
 cd .dafny/dafny/
 git fetch
 ../../tools/checkout-dafny-commit.sh
-git pull || /bin/true # don't try to pull if dafny pointer is to a specific commit, not a branch name
+git pull || true # don't try to pull if dafny pointer is to a specific commit, not a branch name
 mono ../boogie/nuget.exe restore Source/Dafny.sln
 msbuild /p:Configuration=Release Source/Dafny.sln
