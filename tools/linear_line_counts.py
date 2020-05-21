c54a5ca9058b961155f084f331d1a2052968a6db
Jon Howell <howell@vmware.com>
2020-05-21 11:33:07 -0700

    linear line counting across branches

---
+++ tools/linear_line_counts.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""
+A hand-tuned script for doing head-to-head line counts of linear component across branches.
+"""
+
+import subprocess
+import line_count_lib
+import line_counter_report_lib
+
+def do_cmd(cmd):
+    print("CMD", " ".join(cmd))
+    subprocess.call(cmd)
+
+class Package:
+    def __init__(self, label, branch, sources):
+        self.label = label
+        self.branch = branch
+        self.sources = sources
+
+    def count(self):
+        do_cmd(["git", "checkout", self.branch])
+        print("NOW ON", cur_branch())
+        for source in self.sources:
+            self.count_one(source)
+
+    def count_one(self, source):
+        counter = line_count_lib.Counter(".")
+        dafnyFile = line_count_lib.DafnyFile(source, 0.0)
+        counter.collect_line_counts([dafnyFile])
+        print("spec",dafnyFile.spec, "impl",dafnyFile.impl, "proof",dafnyFile.proof)
+
+packages = [
+    Package("BTree-linear", branch="eval-btree-linear", sources=
+            ["lib/DataStructures/BtreeModel.i.dfy",
+                "lib/DataStructures/MutableBtree.i.dfy"]),
+    Package("BTree-master", branch="eval-btree-master", sources=
+            ["lib/DataStructures/BtreeModel.i.dfy",
+                "lib/DataStructures/MutableBtree.i.dfy"]),
+    ]
+
+def cur_branch():
+    return subprocess.run("git rev-parse --abbrev-ref HEAD".split(), stdout=subprocess.PIPE).stdout.decode("utf-8").strip()
+
+def main():
+    start_branch = cur_branch()
+
+    try:
+        for package in packages:
+            package.count()
+    finally:
+        do_cmd(["git", "checkout", start_branch])
+        
+main()
