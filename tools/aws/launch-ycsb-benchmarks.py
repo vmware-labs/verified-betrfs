309904c75a6a045ab63a09dfe74d5bfec8b08cac
Andrea Lattuada <andrea@lattuada.me>
2020-05-25 08:15:44 +0200

    break run-veri-config more, #6

--- tools/aws/launch-ycsb-benchmarks.py
+++ tools/aws/launch-ycsb-benchmarks.py
@@ -4,49 +4,52 @@ from automation import *
 from suite import *
 
 common_vars = [
-    Variable("git_branch", "git_branch", [Value("master",    "master")]),
     Variable("cgroup",     "run_veri",   [Value("yescgroup", "cgroup=True")]),
-    Variable("ram",        "run_veri",   [Value("2gb",       "ram=2.0gb")]),
+    Variable("ram",        "run_veri",   [Value("2gb",       "ram=4.0gb")]),
     Variable("device",     "run_veri",   [Value("disk",      "device=disk")]),
     Variable("workload",   "run_veri",
-             [Value("all", "workload=\"" +
-                    "ycsb/workloada-onefield.spec " + # load
-                    "ycsb/workloadc-uniform.spec"   + # Doesn't modify database, so sneak it in here
-                    "ycsb/workloada-onefield.spec " + # runs...
-                    "ycsb/workloadb-onefield.spec " +
-                    "ycsb/workloadc-onefield.spec " +
-                    "ycsb/workloadd-onefield.spec " +
-                    "ycsb/workloade-onefield.spec " +
-                    "ycsb/workloadf-onefield.spec\""),]),
+             [Value("all", "workload=" + ",".join(
+                    ["ycsb/workloada-onefield.spec", # load
+                    "ycsb/workloadc-uniform.spec"  , # Doesn't modify database, so sneak it in here
+                    "ycsb/workloada-onefield.spec" , # runs...
+                    "ycsb/workloadb-onefield.spec" ,
+                    "ycsb/workloadc-onefield.spec" ,
+                    "ycsb/workloadd-onefield.spec" ,
+                    "ycsb/workloade-onefield.spec" ,
+                    "ycsb/workloadf-onefield.spec" ,
+                    ])),]),
     #Variable("duration", "run_veri", [Value("2h", "time_budget=2h")]),
-    Variable("replica",  "silent", [Value("r0", "r=0"),
-                                    Value("r1", "r=1"),
-                                    Value("r2", "r=2"),
-                                    Value("r3", "r=3"),
-                                    Value("r4", "r=4")]),
+    Variable("replica",  "silent", [Value("r0", "r=0")]),
     ]
 veri_suite = Suite(
     "veribetrkv",
+    Variable("git_branch", "git_branch", [
+        Value("master",    "master"),
+        Value("linear",    "linear-disintegration"),
+        ]),
     Variable("system",     "run_veri", [Value("veri", "veri")]),
-    Variable("rowcache",   "run_veri", [Value("norowcache", ""),
-                                        Value("yesrowcache", "use_unverified_row_cache")]),
+    Variable("rowcache",   "run_veri", [Value("norowcache", "")]),
     *common_vars)
+
+common_vars_others = common_vars + [
+    Variable("git_branch", "git_branch", [Value("master",    "master")]),
+    ]
+
 rocks_suite = Suite(
     "rocksdb",
     Variable("system",  "run_veri", [Value("rocks", "rocks")]),
-    Variable("filters", "run_veri", [Value("nofilters", ""),
-                                     Value("yesfilters", "use_filters")]),
-    *common_vars)
+    Variable("filters", "run_veri", [Value("nofilters", "")]),
+    *common_other_vars)
 berkeley_suite = Suite(
     "berkeleydb",
     Variable("system", "run_veri", [Value("berkeley", "berkeley")]),
-    *common_vars)
+    *common_other_vars)
 kyoto_suite = Suite(
     "berkeleydb",
     Variable("system", "run_veri", [Value("kyoto", "kyoto")]),
-    *common_vars)
+    *common_other_vars)
 #suite = ConcatSuite("ycsb-001", veri_suite, rocks_suite, berkeleydb_suite)
-suite = ConcatSuite("ycsb-berkeley-000", berkeley_suite)
+suite = ConcatSuite("ycsb-veri-new-script-003", veri_suite)
 
 RUN_VERI_PATH="tools/run-veri-config-experiment.py"
 
@@ -58,6 +61,7 @@ def cmd_for_idx(idx, worker):
         ]
         + [RUN_VERI_PATH] + variant.run_veri_params() + ["output=../"+variant.outfile()]
         )
+    print(cmd)
     return Command(str(variant), cmd)
 
 def main():
@@ -67,8 +71,8 @@ def main():
 
     workers = retrieve_running_workers()
     blacklist = []
-    works = [w for w in workers if w["Name"] not in blacklist]
-    worker_pipes = launch_worker_pipes(workers, len(suite.variants), cmd_for_idx, dry_run=True)
+    # works = [w for w in workers if w["Name"] not in blacklist]
+    worker_pipes = launch_worker_pipes(workers, len(suite.variants), cmd_for_idx, dry_run=False)
     monitor_worker_pipes(worker_pipes)
 
 main()
