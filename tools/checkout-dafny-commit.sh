36e4c7ac627a0236d21409f5cbb3580cdd05d531
Bryan Parno <parno@cmu.edu>
2020-05-24 22:33:38 -0400

    Update Dafny hash

--- tools/checkout-dafny-commit.sh
+++ tools/checkout-dafny-commit.sh
@@ -20,4 +20,4 @@ set -x
 
 # https://github.com/secure-foundations/dafny.git
 # cpp branch
-git checkout 02f16ad6fd33d9c0f1396b790a38ebb1b2412ab2
+git checkout b570703f6948293dc58f34d1a2a157dd1a4f90b7
