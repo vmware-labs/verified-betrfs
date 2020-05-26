dfd132c5f7d3ae58f0877d0e46539b84c4fa8018
Andrea Lattuada <andrea@lattuada.me>
2020-05-26 13:32:02 +0200

    tmp

--- tools/aws/launch-mutablemap.py
+++ tools/aws/launch-mutablemap.py
@@ -41,6 +41,15 @@ def main():
     log("VARIANTS %s" % suite.variants)
 
     workers = retrieve_running_workers()
+    blacklist = [
+        "veri-worker-b00",
+        "veri-worker-b01",
+        "veri-worker-b02",
+        "veri-worker-b03",
+        "veri-worker-b04",
+        "veri-worker-b05",
+    ]
+    workers = [w for w in workers if w["Name"] not in blacklist]
     worker_pipes = launch_worker_pipes(workers, len(suite.variants), cmd_for_idx, dry_run=False)
     monitor_worker_pipes(worker_pipes)
 
