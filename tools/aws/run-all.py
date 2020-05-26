4bac5f69a591f6e71da4d0a3e16a1df9ab6092ac
Andrea Lattuada <andrea@lattuada.me>
2020-05-26 13:18:52 +0200

    more

--- tools/aws/run-all.py
+++ tools/aws/run-all.py
@@ -2,12 +2,18 @@
 
 import sys
 from automation import *
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--ssd', action='store_true', help="select ssd machines")
+parser.add_argument('command')
+args = parser.parse_args()
 
 def cmd_for_idx(idx, worker):
-    return Command("run", ssh_cmd_for_worker(worker) + sys.argv[1:])
+    return Command("run", ssh_cmd_for_worker(worker) + [args.command])
 
 def main():
-    workers = retrieve_running_workers(ssd=True)
+    workers = retrieve_running_workers(ssd=args.ssd)
     blacklist = []
     # blacklist = [
     # ]
