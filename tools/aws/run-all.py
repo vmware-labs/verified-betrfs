367b23f955f30b7a99ef083bcf1ff9127713a168
Andrea Lattuada <andrea@lattuada.me>
2020-05-26 21:35:11 +0200

    script updates

--- tools/aws/run-all.py
+++ tools/aws/run-all.py
@@ -6,6 +6,7 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--ssd', action='store_true', help="select ssd machines")
+parser.add_argument('--dry-run', action='store_true')
 parser.add_argument('command')
 args = parser.parse_args()
 
@@ -14,11 +15,17 @@ def cmd_for_idx(idx, worker):
 
 def main():
     workers = retrieve_running_workers(ssd=args.ssd)
-    blacklist = []
-    # blacklist = [
-    # ]
+    # blacklist = []
+    blacklist = [
+        "veri-worker-b00", 
+        "veri-worker-b01", 
+        "veri-worker-b02", 
+        "veri-worker-b03", 
+        "veri-worker-b04", 
+        "veri-worker-b05", 
+    ]
     workers = [w for w in workers if w["Name"] not in blacklist]
-    worker_pipes = launch_worker_pipes(workers, len(workers), cmd_for_idx, dry_run=False)
+    worker_pipes = launch_worker_pipes(workers, len(workers), cmd_for_idx, dry_run=args.dry_run)
     monitor_worker_pipes(worker_pipes)
 
 main()
