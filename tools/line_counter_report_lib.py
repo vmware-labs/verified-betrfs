bd81115fdd41e0b66bd0c37f9bfcb8b373a65641
Andrea Lattuada <andrea@lattuada.me>
2020-05-27 10:05:41 +0200

    master line counter

--- tools/line_counter_report_lib.py
+++ tools/line_counter_report_lib.py
@@ -3,6 +3,11 @@ import lib_deps
 import json
 import os
 
+# make build/Impl/Bundle.i.lcreport -j4
+# (incremental report redraw:)
+# tools/line_counter.py --mode report --input Impl/Bundle.i.dfy --output build/Impl/Bundle.i.lcreport
+# cp build/Impl/Bundle.i.lcreport ../veripapers/osdi2020/data/line-counts.tex
+
 def loadReport(iref):
     fp = open(lib_deps.targetName(iref, ".lc"))
     values = json.load(fp)
@@ -19,7 +24,7 @@ def gatherReports(input):
 def accumulate(reports, mapper):
     counters = {}
     def getCounter(report):
-        key = mapper(report)
+        key = mapper.key(report)
         if key not in counters:
             counters[key] = collections.Counter()
         return counters[key]
@@ -29,9 +34,44 @@ def accumulate(reports, mapper):
         del counter["source"]
     return counters
 
+row_labels = {
+    "ManualMapper-map": "Map, 3States\\template{Map}",
+    "ManualMapper-abstractbetree": "Abstract{\\beptree}",
+    "ManualMapper-betree": "{\\beptree}",
+    "ManualMapper-compositeviewmap": "CompositeViewMap",
+    "ManualMapper-betreejournaliosystem": "{\\beptree}{\iosystem}",
+    "ManualMapper-concreteiosystem": "Concrete{\iosystem}",
+    "ManualMapper-impl": "implementation code",
+    "ManualMapper-lib": "libraries",
+        }
+
+def write_tex_table(fp, counters):
+    fp.write("\\begin{tabular}{|l|rrr|}\n")
+    fp.write("\\hline\n")
+    fp.write("Major component & spec & impl & proof \\\\\n")
+    fp.write("\\hline\n")
+
+    def write_row(label, rowdata):
+        fp.write("%s & %d & %d & %d \\\\\n" % (
+                label,
+                rowdata["spec"],
+                rowdata["impl"],
+                rowdata["proof"]
+                ))
+
+    keys = list(counters.keys())
+    keys.sort()
+    for key,label in row_labels.items():
+        label = row_labels[key]
+        write_row(label, counters[key])
+    fp.write("\\hline\n")
+    write_row("total", counters["AllMapper-all"])
+    fp.write("\\hline\n")
+    fp.write("\\end{tabular}\n")
+
 def write_table(fp, counters):
     def write_row(key, rowdata):
-        fp.write("# %-20s %5s %5s %5s\n" % (key,
+        fp.write("%% %-30s %5s %5s %5s\n" % (key,
                 rowdata["spec"],
                 rowdata["impl"],
                 rowdata["proof"]))
@@ -41,11 +81,25 @@ def write_table(fp, counters):
     for key in keys:
         write_row(key, counters[key])
 
-def report(input, output):
-    reports = gatherReports(input)
+class Mapper:
+    def __init__(self):
+        self.categ = self.__class__.__name__
 
-    counters = accumulate(reports, lambda r: "all")
-    def categorize(report):
+    def key(self, report):
+        return "%s-%s" % (self.categ, self.map(report))
+
+class AllMapper(Mapper):
+    def __init__(self):
+        super().__init__()
+
+    def map(self, path):
+        return "all"
+
+class DirMapper(Mapper):
+    def __init__(self):
+        super().__init__()
+
+    def map(self, report):
         source = report["source"]
         dirpart = os.path.dirname(source)
         if dirpart == "Impl":
@@ -58,10 +112,33 @@ def report(input, output):
                 return "Impl-Misc"
         else:
             return dirpart
-    counters.update(accumulate(reports, categorize))
+
+class ManualMapper(Mapper):
+    def __init__(self):
+        super().__init__()
+        self.mapping = {}
+        for line in open("docs/file-classifications.txt").readlines():
+            categ,path = line.split()
+            if path.startswith("./"):
+                path = path[2:]
+            self.mapping[path] = categ
+
+    def map(self, report):
+        source = report["source"]
+        if source not in self.mapping:
+            print("unmapped: ", source)
+        return self.mapping.get(report["source"], "unmapped")
+
+def report(input, output):
+    reports = gatherReports(input)
+
+    counters = accumulate(reports, AllMapper())
+    counters.update(accumulate(reports, DirMapper()))
+    counters.update(accumulate(reports, ManualMapper()))
     fp = open(output, "w")
-    json.dump(counters, fp, sort_keys=True, indent=2)
-    fp.write("\n")
+#    json.dumps(counters, fp, sort_keys=True, indent=2)
+#    fp.write("\n")
+    write_tex_table(fp, counters)
     write_table(fp, counters)
     fp.close()
 
