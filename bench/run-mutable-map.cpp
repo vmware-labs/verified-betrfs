5431490b6de2d753a156a67a6400564062ca59e2
Travis Hance <tjhance7@gmail.com>
2020-09-03 17:06:46 -0400

    fix warning about printf types

--- bench/run-mutable-map.cpp
+++ bench/run-mutable-map.cpp
@@ -7,7 +7,7 @@ int main(int argc, char**argv) {
     }
     uint64 seed = atol(argv[1]);
     bool dry = (strcmp(argv[2], "true") == 0);
-    printf("METADATA title running with seed %llu dry run %d\n", seed, dry);
+    printf("METADATA title running with seed %llu dry run %d\n", (unsigned long long)seed, dry);
     MutableMapBench_Compile::__default::Run(seed, dry);
     return 0;
 }
