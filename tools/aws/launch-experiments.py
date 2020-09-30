3d4f9cdf489af13f46b3f6ae4bd148b4060b4b91
Rob Johnson <rob@robjohnson.io>
2020-09-29 17:54:33 -0700

    got it working, it seems

--- tools/aws/launch-experiments.py
+++ tools/aws/launch-experiments.py
@@ -8,14 +8,14 @@ common_vars = [
     Variable("ram", "run_veri", [Value("2gb", "ram=2.0gb")]),
     Variable("bucketWeight", "run_veri", [Value("bw", "bucketWeight=2031616")]),
     Variable("device", "run_veri", [Value("disk", "device=disk")]),
-    Variable("workload", "run_veri", [Value("wkc", "workload=ycsb/workloadc-big.spec,ycsb/workloadc-big.spec")]),
+    Variable("workload", "run_veri", [Value("wkc", "workload=ycsb/workloada-onefield.spec")]),
     Variable("duration", "run_veri", [Value("90m", "time_budget=90m")]),
 #    Variable("replica", "silent", [Value("r0", "r=0"), Value("r1", "r=1")]),
     ]
 veri_suite = Suite(
     "veri",
     #Variable("git_branch", "git_branch", [Value("dynamic-frames", "osdi20-artifact-dynamic-frames"), Value("linear", "osdi20-artifact-linear")]),
-    Variable("git_branch", "run_veri", [Value("row-cache-adventure", "git_branch=row-cache-adventure")]),
+    Variable("git_branch", "run_veri", [Value("osdi20-artifact-linear", "git_branch=osdi20-artifact-linear")]),
     #Variable("row_cache", "run_veri", [
     #    Value("%di" % bi, "env:ROW_CACHE_SIZE=%d cacheSize=%d" % (1<<bi, 0.7*(1<<30) - (1<<bi)*600)) for bi in [3, 12, 15, 18, 21]]),
     Variable("cache_size", "run_veri", [Value("cache200nodes", "cacheSizeInNodes=200")]),
@@ -30,7 +30,7 @@ veri_suite = Suite(
 #    Variable("git_branch", "git_branch", [Value("la2", "leak-adventure-2")]),
 #    Variable("system", "run_veri", [Value("rocks", "rocks")]),
 #    *common_vars)
-suite = ConcatSuite("infra-test-010", veri_suite)
+suite = ConcatSuite("infra-test-012", veri_suite)
 
 RUN_VERI_PATH="tools/run-veri-config-experiment.py"
 
