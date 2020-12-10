2fba808154e9880ad950c9f9928ddb4abc9bfe06
Andrea Lattuada <andrea.lattuada@inf.ethz.ch>
2020-12-10 20:57:35 +0100

    initial notes for aliasing reasoning in linear type system

---
+++ docs/oxide/dynamic-frames/rcs.rs
@@ -0,0 +1,42 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+// encapsulated interior mutability
+
+struct Datum {
+    val: u64,
+}
+
+struct StructA {
+    val: Rc<RegionA, RefCell<Datum>>,
+    left: Option<Box<StructA>>,
+    right: Option<Box<StructB>>,
+}
+
+struct StructB {
+    val: Rc<RegionA, RefCell<Datum>>,
+    left: Option<Box<StructA>>,
+    right: Option<Box<StructB>>,
+}
+
+fn main() {
+
+    let a = Rc::new(RefCell::new(Object::new())); 
+    assert something_is_true(a);
+    let b = a.clone();
+    assert something_is_true(b);
+
+    do_something("REALLY shared" a); // immutable bit
+
+    assert something_is_true(b); // not true anymore!!!
+
+
+
+
+    // unsafe {
+    //     let aref = &mut a;
+    //     let aptr = aref as *mut Object; // raw pointer, untracked by borrow checker
+    //     let b = aptr as &mut Object;
+    //     let c = aptr as &mut Object; // undefined behaviour
+    // }
+}
