5f345ab60f087676cde28699a1fe3ca47540f0d1
Yi Zhou <yizhou7@outlook.com>
2021-01-31 22:23:37 -0500

    should have added the python file instead

---
+++ lib/Marshalling/test.py
@@ -0,0 +1,17 @@
+import jinja2
+import sys
+
+try:
+    count = int(sys.argv[1])
+    assert count >= 2
+except:
+    count = 2
+
+templateLoader = jinja2.FileSystemLoader(searchpath="./")
+templateEnv = jinja2.Environment(loader=templateLoader)
+TEMPLATE_FILE = 'test.jinja2'
+t = templateEnv.get_template(TEMPLATE_FILE)
+
+esize = hex(0x1_0000_0000_0000_0000 // (count-1))
+
+print(t.render(count=count, esize=esize))
