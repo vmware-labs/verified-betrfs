d545d7416052f065b2e330b4c31979583238a7ff
Yi Zhou <yizhou7@outlook.com>
2021-02-05 12:12:03 -0500

    starting union marshalling

--- lib/Marshalling/gen.py
+++ lib/Marshalling/gen.py
@@ -2,14 +2,22 @@ import jinja2
 import sys
 
 try:
-    count = int(sys.argv[1])
+    count = int(sys.argv[2])
+    target = sys.argv[1]
     assert count >= 2
+    assert target in {"tuple", "union"}
 except:
-    count = 2
+    print(f"example usage: python {sys.argv[0]} tuple 2")
+    sys.exit(0)
 
 templateLoader = jinja2.FileSystemLoader(searchpath="./")
 templateEnv = jinja2.Environment(loader=templateLoader)
-TEMPLATE_FILE = 'tuple.jinja2'
+
+if target == "tuple":
+    TEMPLATE_FILE = 'tuple.jinja2'
+else:
+    TEMPLATE_FILE = 'union.jinja2'
+
 t = templateEnv.get_template(TEMPLATE_FILE)
 
 esize = hex(0x1_0000_0000_0000_0000 // (count-1))
