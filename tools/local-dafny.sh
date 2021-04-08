c2f9cce44aa0f1bd1acfee18819090854bb85a98
Andrea Lattuada <andrea.lattuada@inf.ethz.ch>
2021-04-08 18:01:45 +0200

    add NLarith warning to local-dafny

--- tools/local-dafny.sh
+++ tools/local-dafny.sh
@@ -4,6 +4,7 @@
 # SPDX-License-Identifier: BSD-2-Clause
 
 
+echo "NOTE: this will use the /noNLarith flag! Don't use this if you're working on a file where NLarith is allowed in the Makefile"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 set -x
 $DIR/../.dafny/bin/dafny "$@" /trace /induction:1 /noNLarith
