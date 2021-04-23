89beb89c6040f21b4e38d26e02136a84905a7279
Bryan Parno <parno@cmu.edu>
2021-04-23 17:22:28 -0400

    Fix part of the framework

--- framework/LinearExtern.h
+++ framework/LinearExtern.h
@@ -136,7 +136,7 @@ Tuple0 seq_free(linear_seq<A> s) {
 template <typename A>
 DafnySequence<A> seq_unleash(linear_seq<A> s) {
   DafnySequence<A> ret(*s);  // TODO: Copies contents of s into ret
-  seq_free(*s);
+  seq_free(s);
   return ret;
 }
 
