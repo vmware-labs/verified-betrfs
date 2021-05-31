f861bd14de25887e0be337028ea2a610f4f2f0da
Andrea Lattuada <andrea.lattuada@inf.ethz.ch>
2021-05-31 17:01:44 +0200

    Remove old verichecks code

--- tools/verichecks/bastion/init.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-# Copyright 2018-2021 VMware, Inc., Microsoft Inc., Carnegie Mellon University, ETH Zurich, and University of Washington
-# SPDX-License-Identifier: BSD-2-Clause
-
-
-cd /root/verichecks
-. ./venv3/bin/activate
-python3 server.py
--- tools/verichecks/worker/maybeshutdown.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-# Copyright 2018-2021 VMware, Inc., Microsoft Inc., Carnegie Mellon University, ETH Zurich, and University of Washington
-# SPDX-License-Identifier: BSD-2-Clause
-
-
-ls -1 /home/ubuntu/verichecks-worker/worker-dir/shutdown && rm /home/ubuntu/verichecks-worker/worker-dir/shutdown && sudo shutdown --poweroff
