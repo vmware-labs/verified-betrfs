fa0d8c1e8d5a05ae905f78961f548346e7d64620
Jon Howell <howell@vmware.com>
2022-01-14 12:31:24 -0800

    local-dafny filters out cruft, keeps colors

--- tools/local-dafny.sh
+++ tools/local-dafny.sh
@@ -7,4 +7,8 @@
 echo "NOTE: using /noNLarith and /compile:0"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 set -x
-$DIR/../.dafny/bin/dafny "$@" /induction:1 /noNLarith /compile:0 /timeLimit:20
+# if you don't have unbuffer, apt-get install expect
+# unbuffer convinces dafny to emit colored errors even when stdout is a pipe.
+# stdbuf -o0 and --line-buffered are my belt-and-suspenders attempts to keep dafny
+# output displayed as soon as it's available.
+unbuffer $DIR/../.dafny/bin/dafny "$@" /induction:1 /noNLarith /compile:0 /timeLimit:20 | stdbuf -o0 egrep --line-buffered -v '(^\s*\(|anon)'
