747d04b1ca8280de21b1e25893409165c173f71a
Ryan Stutsman <stutsman@cs.utah.edu>
2023-04-14 07:51:23 -0600

    Hack to fix to compile of BundleVSpace.i.cpp

--- concurrency/node-replication/compile-bench.sh
+++ concurrency/node-replication/compile-bench.sh
@@ -5,6 +5,9 @@ set -e
 for n_replicas in 1 2 3 4; do
     make clean
     sed -i "s/\(NUM_REPLICA.*:= \).*;/\1${n_replicas};/" Constants.i.dfy
+    make BundleVSpace.i.cpp || true
+    # Needed until lseq_alloc_raw_hugetables compile issue is resolved.
+    sed -i 's/^ *\(s = LinearExtern::lseq_alloc_raw_hugetables\)/\/\/ \1/' BundleVSpace.i.cpp
     make || true
     [[ -f "app" ]] && mv app app-${n_replicas}replicas
 done
