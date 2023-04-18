2b6584a28c014d52fd4a09ba98fd86341df841a3
Travis Hance <tjhance7@gmail.com>
2023-04-17 21:17:40 -0400

    fix

--- build-cpp-source.sh
+++ build-cpp-source.sh
@@ -3,7 +3,7 @@
 ### build SplinterCache source
 
 cd concurrency/scache
-dafny /trace /compile:0 /induction:1 /noNLarith /noVerify /spillTargetCode:3 /compileTarget:cpp /countVerificationErrors:0 Application.i.dfy Extern.h LinearExtern.h DiskExtern.h
+../../tools/local-dafny.sh /trace /compile:0 /induction:1 /noNLarith /noVerify /spillTargetCode:3 /compileTarget:cpp /countVerificationErrors:0 Application.i.dfy Extern.h LinearExtern.h DiskExtern.h
 
 mkdir -p ../../build/cache-source/
 cp Application.i.cpp ../../build/cache-source/
