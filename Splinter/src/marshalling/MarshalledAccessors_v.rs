0c93c07afd35a02ae96f771c10e5fea016542016
Jon Howell <howell@vmware.com>
2024-01-02 15:55:08 -0800

    a factoring of integer marshalling that\'s okay

--- Splinter/src/marshalling/MarshalledAccessors_v.rs
+++ Splinter/src/marshalling/MarshalledAccessors_v.rs
@@ -75,6 +75,10 @@ pub trait Marshalling<U> : Premarshalling<U> {
     ;
 }
 
+//////////////////////////////////////////////////////////////////////////////
+// Integer marshalling
+//////////////////////////////////////////////////////////////////////////////
+
 trait NativePackedInt {
     type IntType;
 
@@ -95,6 +99,41 @@ trait NativePackedInt {
 
 struct PackedIntPremarshalling<U: NativePackedInt> {
     _p: std::marker::PhantomData<(U,)>,
+}
+
+
+impl<U> PackedIntPremarshalling<U> where U: NativePackedInt {
+    exec fn install_bytes(&self, source: &Vec<u8>, data: &mut Vec<u8>, start: u64) -> (end: u64)
+    requires
+        start + source.len() <= old(data).len(),
+    ensures
+        end == start + source.len(),
+        forall |i| 0 <= i < start ==> data[i] == old(data)[i],
+        forall |i| 0 <= i < source.len() ==> data[start as int + i] == source[i],
+        forall |i| end <= i < data.len() ==> data[i] == old(data)[i],
+    {
+        // Copy the vector byte-by-byte into place
+        let count = source.len() as usize;
+        let end = start as usize + count;
+        let mut k:usize = 0;
+        while k < count
+        invariant
+            end == start + source.len(),
+            end <= data.len(),  // manual-every-effing-invariant blows
+            data.len() == old(data).len(),  // manual-every-effing-invariant blows
+            source.len() == count,  // manual-every-effing-invariant blows. source,count aren't even mutable!
+            forall |i| 0 <= i < start ==> data[i] == old(data)[i],
+            forall |i| 0 <= i < k ==> data[start as int + i] == source[i],
+            forall |i| end <= i < data.len() ==> data[i] == old(data)[i],
+        {
+            //data[k] = s[k];
+            // Do we want some sort of intrinsic so we don't have to copy u32s a byte at a time!?
+            data.set(start as usize + k, source[k]);
+            k += 1;
+        }
+        assert( data@.subrange(start as int, end as int) =~= source@ );  // extensionality: it's what's for ~.
+        end as u64
+    }
 }
 
 impl<U> Premarshalling<U::IntType> for PackedIntPremarshalling<U> where U: NativePackedInt {
@@ -161,32 +200,12 @@ impl Marshalling<u32> for PackedIntPremarshalling<u32> {
     exec fn marshall(&self, value: &u32, data: &mut Vec<u8>, start: u64) -> (end: u64)
     {
         // TODO this interface from verus pervasive bytes.rs can't be fast...
-
         // Marshal the int into a local vector
         let s = u32_to_le_bytes(*value);
         proof { lemma_auto_spec_u32_to_from_le_bytes(); }
         assert( s@.subrange(0, 4) =~= s@ ); // need a little extensionality? Do it by hand!
 
-        // Copy the vector byte-by-byte into place
-        let end = start + 4;
-        let mut k:usize = 0;
-        while k < 4
-        invariant
-            end == start + self.size(value),
-            end <= data.len(),  // manual-every-effing-invariant blows
-            data.len() == old(data).len(),  // manual-every-effing-invariant blows
-            s.len() == 4,  // manual-every-effing-invariant blows
-            forall |i| 0 <= i < start ==> data[i] == old(data)[i],
-            forall |i| 0 <= i < k ==> data[start as int + i] == s[i],
-            forall |i| end <= i < data.len() ==> data[i] == old(data)[i],
-        {
-            //data[k] = s[k];
-            // Do we want some sort of intrinsic so we don't have to copy u32s a byte at a time!?
-            data.set(start as usize + k, s[k]);
-            k += 1;
-        }
-        assert( data@.subrange(start as int, end as int) =~= s@ );  // extensionality: it's what's for ~.
-        end
+        self.install_bytes(&s, data, start)
     }
 }
 
@@ -200,104 +219,37 @@ impl NativePackedInt for u64 {
     exec fn exec_size() -> (sz: u64) { 8 }
 }
 
-// struct NativePackedIntU64 {}
-// 
-// impl NativePackedInt<u64> for NativePackedIntU64 {
-//     spec fn spec_size() -> u64 { 8 }
-// 
-//     proof fn spec_size_ensures()
-//     ensures
-//         0 < Self::spec_size() <= 8
-//     {}
-// 
-// 
-//     exec fn exec_size() -> (sz: u64)
-//     ensures
-//         sz == Self::spec_size()
-//     { 8 }
-// }
-// 
-// struct U64Marshalling {}
-// 
-// impl Marshalling<u64> for U64Marshalling {
-//     //////////////////////////////////////////////////////////////////////
-//     // Copy-paste commonality I don't know how to factor out
-//     spec fn valid(&self) -> bool
-//     {
-//         true
-//     }
-// 
-//     spec fn parsable(&self, data: Seq<u8>) -> bool
-//     {
-//         /*std::mem::size_of<u32>()*/
-//         NativePackedIntU64::spec_size() <= data.len()
-//     }
-// 
-//     fn exec_parsable(&self, data: &Vec<u8>) -> (p: bool)
-//     {
-//         NativePackedIntU64::exec_size() <= data.len() as u64
-//     }
-// 
-//     spec fn marshallable(&self, value: &u64) -> bool
-//     {
-//         true
-//     }
-// 
-//     spec fn size(&self, value: &u64) -> u64
-//     {
-//         NativePackedIntU64::spec_size()
-//     }
-// 
-//     fn exec_size(&self, value: &u64) -> (sz: u64)
-//     {
-//         NativePackedIntU64::exec_size()
-//     }
-//     //
-//     //////////////////////////////////////////////////////////////////////
-// 
-//     spec fn parse(&self, data: Seq<u8>) -> u64
-//     {
-//         spec_u64_from_le_bytes(data.subrange(0, 8))
-//     }
-// 
-//     exec fn try_parse(&self, slice: Slice, data: &Vec<u8>) -> (ov: Option<u64>)
-//     {
-//         if 8 <= data.len() {
-//             Some(u64_from_le_bytes(slice_subrange(data.as_slice(), 0, 8)))
-//         } else {
-//             None
-//         }
-//     }
-// 
-//     exec fn marshall(&self, value: &u64, data: &mut Vec<u8>, start: u64) -> (end: u64)
-//     {
-//         // TODO this interface from verus pervasive bytes.rs can't be fast...
-//         let s = u64_to_le_bytes(*value);
-//         proof { lemma_auto_spec_u64_to_from_le_bytes(); }
-//         assert( s@.subrange(0, 8) =~= s@ ); // need a little extensionality? Do it by hand!
-// 
-//         let end = start + 8;
-//         let mut k:usize = 0;
-//         while k < 8
-//         invariant
-//             end == start + self.size(value),
-//             end <= data.len(),  // manual-every-effing-invariant blows
-//             data.len() == old(data).len(),  // manual-every-effing-invariant blows
-//             s.len() == 8,  // manual-every-effing-invariant blows
-//             forall |i| 0 <= i < start ==> data[i] == old(data)[i],
-//             forall |i| 0 <= i < k ==> data[start as int + i] == s[i],
-//             forall |i| end <= i < data.len() ==> data[i] == old(data)[i],
-//         {
-//             //data[k] = s[k];
-//             // Do we want some sort of intrinsic so we don't have to copy u64s a byte at a time!?
-//             data.set(start as usize + k, s[k]);
-//             k += 1;
-//         }
-//         assert( data@.subrange(start as int, end as int) =~= s@ );  // extensionality: it's what's for ~.
-//         end
-//     }
-// }
-// 
+impl Marshalling<u64> for PackedIntPremarshalling<u64> {
+    open spec fn parse(&self, data: Seq<u8>) -> u64
+    {
+        spec_u64_from_le_bytes(data.subrange(0, 8))
+    }
+
+    exec fn try_parse(&self, slice: Slice, data: &Vec<u8>) -> (ov: Option<u64>)
+    {
+        if 8 <= data.len() {
+            Some(u64_from_le_bytes(slice_subrange(data.as_slice(), 0, 8)))
+        } else {
+            None
+        }
+    }
+
+    exec fn marshall(&self, value: &u64, data: &mut Vec<u8>, start: u64) -> (end: u64)
+    {
+        // TODO this interface from verus pervasive bytes.rs can't be fast...
+        // Marshal the int into a local vector
+        let s = u64_to_le_bytes(*value);
+        proof { lemma_auto_spec_u64_to_from_le_bytes(); }
+        assert( s@.subrange(0, 8) =~= s@ ); // need a little extensionality? Do it by hand!
+
+        self.install_bytes(&s, data, start)
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////////
+// Sequence marshalling
+//////////////////////////////////////////////////////////////////////////////
+
 // pub trait SeqMarshalling<U, Elt: Marshalling<U>> : Marshalling<Vec<U>> {
 //     spec fn spec_elt(&self) -> Elt
 //     ;
