dac1064fbc3209c150a4a6dd1ea87dfd29448027
Jon Howell <howell@vmware.com>
2024-01-15 14:53:06 -0800

    update license header

--- Splinter/tools/check_recordings/src/main.rs
+++ Splinter/tools/check_recordings/src/main.rs
@@ -1,3 +1,5 @@
+// Copyright 2018-2023 VMware, Inc., Microsoft Inc., Carnegie Mellon University, ETH Zurich, University of Washington
+// SPDX-License-Identifier: BSD-2-Clause
 extern crate git2;
 
 use chrono::TimeZone;
