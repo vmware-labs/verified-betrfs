4359a786764020dd2befbfc0cd97c115cb3cf170
Jon Howell <howell@vmware.com>
2024-03-08 18:38:32 -0800

    verus_fmt

--- Splinter/src/marshalling.rs
+++ Splinter/src/marshalling.rs
@@ -1,9 +1,9 @@
 // Copyright 2018-2023 VMware, Inc., Microsoft Inc., Carnegie Mellon University, ETH Zurich, University of Washington
 // SPDX-License-Identifier: BSD-2-Clause
-pub mod Slice_v;
-pub mod Marshalling_v;
 pub mod IntegerMarshalling_v;
-pub mod SeqMarshalling_v;
-pub mod UniformSizedSeq_v;
+pub mod Marshalling_v;
 pub mod ResizableUniformSizedSeq_v;
+pub mod SeqMarshalling_v;
+pub mod Slice_v;
+pub mod UniformSizedSeq_v;
 pub mod math_v;
