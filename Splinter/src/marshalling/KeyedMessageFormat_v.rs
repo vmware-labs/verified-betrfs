bc8d1eb387251da317133c6c635cbba0fbbeddaa
Jon Howell <howell@vmware.com>
2024-09-11 13:43:32 -0700

    progress towards implementing Marshal ifc

--- Splinter/src/marshalling/KeyedMessageFormat_v.rs
+++ Splinter/src/marshalling/KeyedMessageFormat_v.rs
@@ -19,8 +19,8 @@ use crate::marshalling::VariableSizedElementSeq_v::*;
 verus! {
 
 pub struct SpecKVPair {
-    pub key: Seq<u8>,
-    pub value: Seq<u8>,
+    pub key: Seq<int>,
+    pub value: Seq<int>,
 }
 
 pub struct KVPair {
@@ -31,7 +31,7 @@ pub struct KVPair {
 impl Deepview<SpecKVPair> for KVPair {
     open spec fn deepv(&self) -> SpecKVPair
     {
-        SpecKVPair{key: self.key@, value: self.value@}
+        SpecKVPair{key: self.key.deepv(), value: self.value.deepv()}
     }
 }
 
@@ -43,24 +43,29 @@ const KEY_SLOT: usize = 0;
 const VALUE_SLOT: usize = 1;
 
 impl KeyedMessageFormat {
-    pub closed spec fn valid(&self) -> bool
+    pub closed spec fn internal_valid(&self) -> bool
     {
         &&& self.vfmt.seq_valid()
+    }
+
+    pub closed spec fn internal_marshallable(&self, key_len: usize, value_len: usize) -> bool
+    {
+        &&& self.internal_valid()
+        &&& Self::fits(key_len, value_len)
+        &&& self.vfmt.total_size() == Self::required_size(key_len, value_len)
     }
 
     // A data container capable of receiving key and value of this len
     pub closed spec fn is_container(&self, data: Seq<u8>, key_len: usize, value_len: usize) -> bool
     {
-        &&& self.valid()
+        &&& self.internal_marshallable(key_len, value_len)
         &&& self.vfmt.total_size() <= data.len()
-        &&& Self::fits(key_len, value_len)
-        &&& self.vfmt.total_size() == Self::required_size(key_len, value_len)
     }
 
     // A data container capable of supplying a key and a value
     pub closed spec fn filled(&self, data: Seq<u8>) -> bool
     {
-        &&& self.valid()
+        &&& self.internal_valid()
         &&& self.vfmt.total_size() <= data.len()
         &&& self.vfmt.gettable(data, KEY_SLOT as int)
         &&& self.vfmt.elt_parsable(data, KEY_SLOT as int)
@@ -102,7 +107,7 @@ impl KeyedMessageFormat {
     requires
         Self::fits(key_data.len(), value_data.len()),
     ensures
-        s.valid(),
+        s.internal_valid(),
         s.size() == Self::required_size(key_data.len(), value_data.len()),
         // TODO ensures we can store_key_value() it with this key and data
     {
@@ -144,18 +149,21 @@ impl KeyedMessageFormat {
         let kmf = Self::new(key_data, value_data);
         let len = kmf.vfmt.exec_total_size();
         let mut data = Self::prealloc(len);
-        kmf.store_key_value(&mut data, key_data, value_data);
+        kmf.store_key_value(&Slice::all(&data), &mut data, key_data, value_data);
         data
     }
 
-    pub exec fn store_key_value(&self, data: &mut Vec<u8>, key_data: &Vec<u8>, value_data: &Vec<u8>)
+    // inner part of marshal
+    pub exec fn store_key_value(&self, slice: &Slice, data: &mut Vec<u8>, key_data: &Vec<u8>, value_data: &Vec<u8>)
     requires
-        self.is_container(old(data)@, key_data.len(), value_data.len()),
+        self.is_container(slice@.i(old(data)@), key_data.len(), value_data.len()),
+        slice@.valid(old(data)@),
     ensures
-        self.filled(data@),
+        self.filled(slice@.i(data@)),
         data@.len() == old(data)@.len(),
+        slice@.agree_beyond_slice(old(data)@, data@),
+        self.parse(slice@.i(data@)) == (SpecKVPair{key: key_data.deepv(), value: value_data.deepv()}),
     {
-        let slice = Slice::all(data);
         proof{ SpecSlice::all_ensures::<u8>() };
         self.vfmt.initialize(&slice, data);
         self.vfmt.exec_append(&slice, data, key_data);
@@ -164,7 +172,8 @@ impl KeyedMessageFormat {
         assert( self.vfmt.appendable(slice@.i(data@), value_data.deepv()) );    // trigger
         self.vfmt.exec_append(&slice, data, value_data);
 
-        assert( slice@.i(data@) == data@ );  // extn equality trigger
+        assume( false );
+//         assert( slice@.i(data@) == data@ );  // extn equality trigger
     }
 
     // TODO Want to borrow the data! Read it in-place, let caller copy as needed.
@@ -182,6 +191,80 @@ impl KeyedMessageFormat {
         self.filled(slice@.i(data@)),
     {
         self.vfmt.exec_get_elt(slice, data, VALUE_SLOT)
+    }
+}
+
+impl Marshal for KeyedMessageFormat {
+    type DV = SpecKVPair;
+    type U = KVPair;
+
+    open spec fn valid(&self) -> bool
+    {
+        self.internal_valid()
+    }
+
+    open spec fn parsable(&self, data: Seq<u8>) -> bool
+    {
+        self.filled(data)
+    }
+
+    exec fn exec_parsable(&self, slice: &Slice, data: &Vec<u8>) -> (p: bool)
+    {
+        assume( false );
+        false   // TODO
+    }
+
+    // TODO rename filled->parsable, is_container -> marshallable?
+    open spec fn marshallable(&self, value: Self::DV) -> bool
+    {
+        self.internal_marshallable(value.key.len() as usize, value.value.len() as usize)
+    }
+
+    open spec fn spec_size(&self, value: Self::DV) -> usize
+    {
+        Self::required_size(value.key.len() as usize, value.value.len() as usize) as usize
+    }
+
+    exec fn exec_size(&self, value: &Self::U) -> (sz: usize)
+    {
+        Self::exec_required_size(value.key.len(), value.value.len())
+    }
+
+    closed spec fn parse(&self, data: Seq<u8>) -> Self::DV
+    {
+        let key = self.vfmt.get_elt(data, KEY_SLOT as int);
+        let value = self.vfmt.get_elt(data, VALUE_SLOT as int);
+        SpecKVPair{key, value}
+    }
+
+    exec fn try_parse(&self, slice: &Slice, data: &Vec<u8>) -> (ov: Option<Self::U>)
+    {
+        let key = self.vfmt.try_get_elt(slice, data, KEY_SLOT);
+        let value = self.vfmt.try_get_elt(slice, data, VALUE_SLOT);
+        if key.is_none() || value.is_none() {
+            None
+        } else {
+            Some(KVPair{key: key.unwrap(), value: value.unwrap()})
+        }
+    }
+
+    exec fn exec_parse(&self, slice: &Slice, data: &Vec<u8>) -> (value: Self::U)
+    {
+        self.try_parse(slice, data).unwrap()
+    }
+
+    // jonh skipping translation of Parse -- does it ever save more than
+    // a cheap if condition?
+
+    exec fn exec_marshall(&self, value: &Self::U, data: &mut Vec<u8>, start: usize) -> (end: usize)
+    {
+        let len = Self::exec_required_size(value.key.len(), value.value.len());
+        let slice = Slice{start: start, end: start + len};
+        self.store_key_value(&slice, data, &value.key, &value.value);
+        let end = start + len;
+        assume( false );
+        assert( self.parse(data@.subrange(start as int, end as int)) == value.deepv() );
+        end
     }
 }
 
