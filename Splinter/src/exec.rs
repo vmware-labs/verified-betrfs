1b7fd592682930e12012e6071f892036d4fb24b5
Jialin Li <jialin_li@utexas.edu>
2024-10-31 13:20:51 -0700

    simple bank example

--- Splinter/src/exec.rs
+++ Splinter/src/exec.rs
@@ -1,2 +1,3 @@
 pub mod MiniAllocator_v;
 pub mod Cache_v;
+pub mod ConcreteCache_v;
--- Splinter/src/exec/Cache_v.rs
+++ Splinter/src/exec/Cache_v.rs
@@ -14,12 +14,13 @@ use crate::spec::MapSpec_t::{ID};
 
 verus!{
 
-pub type Slot = int;
+pub type Slot = i32;
 
 //  Entry is separate from Status because there are some cases
 //  where we need to have shared access to the Entry while modifying
 //  the Status
 pub enum Status {
+    NotFilled,
     Clean,
     Dirty,
     Writeback,
@@ -48,13 +49,12 @@ impl Entry {
     }
 }
 
-
+// TODO: update status_map 
 state_machine!{ Cache {
     fields {
         pub entries: Map<Slot, Entry>,
         pub status_map: Map<Slot, Status>,
         pub lookup_map: Map<Address, Slot>,
-        // pub total_slots: nat,
     }
 
     pub enum Label {
@@ -165,7 +165,8 @@ state_machine!{ Cache {
         update status_map = pre.status_map.union_prefer_right(updated_status_map);
     }}
 
-    // allows for atomically reading and writing a number of slots
+    // NOTE: access must enable batched accesses because program
+    // model needs to make batch updates as an atomic transition
     transition!{ access(lbl: Label) {
         require lbl is Access;
         require forall |slot| #[trigger] lbl->reads.contains_key(slot) 
@@ -239,9 +240,10 @@ state_machine!{ Cache {
  
         let evicted_addrs = Map::new(|slot| evicted_slots.contains(slot), |slot| pre.entries[slot].get_addr()).values();
         let updated_entries = Map::new(|slot| evicted_slots.contains(slot), |slot| Entry::Empty);
+        let updated_status_map = Map::new(|slot| evicted_slots.contains(slot), |slot| Status::NotFilled);
 
         update entries = pre.entries.union_prefer_right(updated_entries);
-        update status_map = pre.status_map.remove_keys(evicted_slots);
+        update status_map = pre.status_map.union_prefer_right(updated_status_map);
         update lookup_map = pre.lookup_map.remove_keys(evicted_addrs);
     }}
 
@@ -250,17 +252,16 @@ state_machine!{ Cache {
     }}
 
     init!{ initialize(slots: nat) {
-        init entries = Map::new(|i: Slot| i < slots , |i| Entry::Empty{});
+        init entries = Map::new(|i: Slot| i < slots , |i| Entry::Empty);
+        init status_map = Map::new(|i: Slot| i < slots , |i| Status::NotFilled);
         init lookup_map = Map::empty();
-        init status_map = Map::empty();
-        // init total_slots = slots;
     }}
 
     // NOTE(disk): 1 outstanding IO for each loading/writeback page, reserved & filled (!writeback) -> 0 I/O
 
     #[invariant]
     pub open spec(checked) fn inv(self) -> bool {
-        &&& self.status_map.dom() <= self.entries.dom()
+        &&& self.status_map.dom() =~= self.entries.dom()
         &&& self.lookup_map.values() <= self.entries.dom()
 
         &&& forall |slot| #[trigger] self.entries.contains_key(slot) && !(self.entries[slot] is Empty) 
@@ -268,7 +269,7 @@ state_machine!{ Cache {
                 &&& self.lookup_map.contains_key(self.entries[slot].get_addr())
                 &&& self.lookup_map[self.entries[slot].get_addr()] == slot
             }
-        
+
         &&& forall |addr| #[trigger] self.lookup_map.contains_key(addr) 
             ==> {
                 &&& !(self.entries[self.lookup_map[addr]] is Empty)
@@ -276,12 +277,13 @@ state_machine!{ Cache {
             }
 
         &&& forall |slot| #[trigger] self.status_map.contains_key(slot)
-            <==> (self.entries.contains_key(slot) && self.entries[slot] is Filled)
+            ==> ( (self.status_map[slot] is NotFilled) <==> !(self.entries[slot] is Filled) )
     }
 
     #[inductive(reserve)]
     fn reserve_inductive(pre: Self, post: Self, lbl: Label, new_slots_mapping: Map<Slot, Address>) { 
         injective_map_property(new_slots_mapping);
+
     }
    
     #[inductive(load_initiate)]
@@ -297,7 +299,7 @@ state_machine!{ Cache {
 
     #[inductive(writeback_initiate)]
     fn writeback_initiate_inductive(pre: Self, post: Self, lbl: Label) { }
-   
+
     #[inductive(writeback_complete)]
     fn writeback_complete_inductive(pre: Self, post: Self, lbl: Label) { }
 
